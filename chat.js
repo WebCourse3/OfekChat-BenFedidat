1f3ebd9e7775df4fc73a69c15d4ce90c540a1753
Ben Fedidat <fedidat@gmail.com>
2017-11-01 13:24:35 +0200

    consistent setColor and others (influence forward and accross clients)

--- chat.js
+++ chat.js
@@ -5,7 +5,25 @@ const io = require('socket.io')(http);
 
 app.use(express.static('static'));
 
-io.on('connection', function(socket, user){
+//[{"ben":[{"color", "red"}, {"bold", "regular"}]}]
+var userStyles = {};
+
+getUserStyle = function(user){
+  var styleString = "";
+  if(userStyles[user]) {
+    for (var style in userStyles[user]) {
+      styleString += style + ": " + userStyles[user][style] + "; ";
+    }
+  }
+  return styleString;
+}
+setUserStyle = function(user, key, value) {
+  if(!userStyles[user])
+    userStyles[user] = {};
+  userStyles[user][key] = value;
+}
+
+io.on('connection', function(socket){
   console.log('a user connected');
   io.emit('user connect event', 'connected');
   socket.on('disconnect', function(){
@@ -13,8 +31,29 @@ io.on('connection', function(socket, user){
     io.emit('user connect event', 'disconnected');
   });
   socket.on('chat message', function(user, msg){
-    console.log('chat message: ' + user + 'said \"' + msg + '"');
-    io.emit('chat message', user, msg);
+    console.log('chat message: ' + user + ' said \"' + msg + '"');
+    var style = getUserStyle(user);
+    io.emit('chat message', user, msg, style);
+  });
+  socket.on('setColor', function(user, value){
+    console.log('setColor: ' + user + ', ' + value);
+    setUserStyle(user, "color", value);
+    io.emit('setColor', user, value);
+  });
+  socket.on('setBold', function(user, value){
+    console.log('setBold: ' + user + ', ' + value);
+    setUserStyle(user, "font-weight", value);
+    io.emit('setBold', user, value);
+  });
+  socket.on('setItalic', function(user, value){
+    console.log('setItalic: ' + user + ', ' + value);
+    setUserStyle(user, "font-weight", value);
+    io.emit('setItalic', user, value);
+  });
+  socket.on('setBorder', function(user, value){
+    console.log('setBorder: ' + user + ', ' + value);
+    setUserStyle(user, "border", value);
+    io.emit('setBorder', user, value);
   });
 });
 
--- static/js/client.js
+++ static/js/client.js
@@ -8,33 +8,15 @@ $(function () {
         `); 
     }
 
-    handleSpecialCommand = function(message) {
+    handleSpecialCommand = function(socket, message) {
         var splitMessage = message.substr(1).split(' ');
-        switch(splitMessage.shift()) {
+        switch(splitMessage[0]) {
             case 'setColor':
-                var currentUser = $('#message-user').val();
-                $('[username=' + currentUser +']').css(
-                    "color", splitMessage[0]
-                );
-                break;
             case 'setBold':
-                var currentUser = $('#message-user').val();
-                $('[username=' + currentUser +']').css(
-                    "font-weight", splitMessage[0]===true?"bold":"normal"
-                );
-                break;
             case 'setItalic':
-                var currentUser = $('#message-user').val();
-                $('[username=' + currentUser +']').css(
-                    "font-style", splitMessage[0]==="true"?"italic":"normal"
-                );
-                break;
             case 'setBorder':
-                var currentUser = $('#message-user').val();
-                $('[username=' + currentUser +']').css(
-                    "border-style", splitMessage[0]
-                );
-                break;              
+                socket.emit(splitMessage[0], $('#message-user').val(), splitMessage[1]);
+                break;
             default:
                 throw("Illegal command");
         }
@@ -50,10 +32,11 @@ $(function () {
         }
         if(messageBody[0] === '/') {
             try{
-                handleSpecialCommand(messageBody);
+                handleSpecialCommand(socket, messageBody);
             } catch(error) {
                 printError(error);
             } finally {
+                $('#message-field').val('');
                 return false;
             }
         }
@@ -62,13 +45,14 @@ $(function () {
         return false;
     });
 
-    socket.on('chat message', function(user, msg){
-        $('#chat').append(`
-            <tr class="row" username="${user}">
+    socket.on('chat message', function(user, msg, style){
+        var appendedMessage = `
+            <tr class="row" username="${user}" style="${style}">
                 <td class="col-2 text-right"><strong>${user + " said: "}<strong></td>
                 <td class="col-10">${msg}</td>
             </tr>
-        `); 
+        `; 
+        $('#chat').append(appendedMessage);
     });
     socket.on('user connect event', function(connectEvent){
         $('#chat').append(`
@@ -77,5 +61,25 @@ $(function () {
                 <td class="col-10">A user ${connectEvent}</td>
             </tr>
         `); 
+    });
+    socket.on('setColor', function(user, value){
+        $('[username=' + user +']').css(
+            "color", value
+        );      
+    });
+    socket.on('setBold', function(user, value){
+        $('[username=' + user +']').css(
+            "font-weight", value
+        );      
+    });
+    socket.on('setItalic', function(user, value){
+        $('[username=' + user +']').css(
+            "font-style", value
+        );      
+    });
+    socket.on('setBorder', function(user, value){
+        $('[username=' + user +']').css(
+            "border-style", value
+        );      
     });
 });
